ee818195f36d0efa36ffbd1bdf28ddd70b146732
Johnson Mbeba <101251043+jjmbeba@users.noreply.github.com>
2024-11-29 06:51:19 +0300

    fix: create-view-form import

--- app/components/reviews/create-review-form.tsx
+++ app/components/reviews/create-review-form.tsx
@@ -6,7 +6,7 @@ import AddReviewButton from "@/app/components/reviews/add-review-button";
 import {z} from "zod";
 import {useForm} from "react-hook-form";
 import {zodResolver} from "@hookform/resolvers/zod";
-import {Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage} from "@/components/ui/form";
+import {Form, FormControl, FormField, FormItem, FormLabel, FormMessage} from "@/components/ui/form";
 import {Input} from "@/components/ui/input";
 import {Button} from "@/components/ui/button";
 
