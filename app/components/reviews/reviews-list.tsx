1f2624265dc508176fbec2825074e3eefc4a02d2
Johnson Mbeba <101251043+jjmbeba@users.noreply.github.com>
2024-12-01 16:16:10 +0300

    Add display box if reviews dont exist

--- app/components/reviews/reviews-list.tsx
+++ app/components/reviews/reviews-list.tsx
@@ -6,20 +6,21 @@ import {RouterOutputs} from "@/trpc/routers/_app";
 import {auth} from "@clerk/nextjs/server";
 import Link from "next/link";
 import {buttonVariants} from "@/components/ui/button";
+import {Ghost} from "lucide-react";
 
 type Props = {
-    recordId:number;
-    mediaType:string;
+    recordId: number;
+    mediaType: string;
 }
 
-const ReviewsList = async ({recordId, mediaType}:Props) => {
+const ReviewsList = async ({recordId, mediaType}: Props) => {
     const reviews = await trpc.reviews.getReviewsByRecordId({
         recordId,
         mediaType
     });
     const {userId} = await auth();
 
-    function hasUserReviewed(userId:string, reviews:RouterOutputs['reviews']['getReviewsByRecordId']){
+    function hasUserReviewed(userId: string, reviews: RouterOutputs['reviews']['getReviewsByRecordId']) {
         return reviews.some((review) => review.userId === userId);
     }
 
@@ -29,18 +30,26 @@ const ReviewsList = async ({recordId, mediaType}:Props) => {
                 <h2 className={'font-bold text-2xl'}>
                     Reviews
                 </h2>
-                {userId ? !hasUserReviewed(userId,reviews) && (<CreateReviewForm recordId={recordId} mediaType={mediaType} />) : (
+                {userId ? !hasUserReviewed(userId, reviews) && (
+                    <CreateReviewForm recordId={recordId} mediaType={mediaType}/>) : (
                     <Link className={buttonVariants({
                         variant: 'link'
                     })} href={'/login'}>
                         Sign in to add review
                     </Link>
-                )  }
+                )}
 
             </div>
-            {reviews.map((review) => (
+            {reviews.length > 0 ? reviews.map((review) => (
                 <ReviewListItem key={review.id} {...review} />
-            ))}
+            )) : (
+                <div className={'mt-16 pb-16 flex items-center justify-center gap-5'}>
+                    <Ghost className={'w-16 h-16'}/>
+                    <p>
+                        Unfortunately, there are no reviews for this record.
+                    </p>
+                </div>
+            )}
         </div>
     )
 }
