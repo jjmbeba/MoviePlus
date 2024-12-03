c264ed076a627e92186fe13ac2a65d8180316d52
Johnson Mbeba <101251043+jjmbeba@users.noreply.github.com>
2024-12-03 23:24:24 +0300

    reformat tabs

--- app/bookmarks/page.tsx
+++ app/bookmarks/page.tsx
@@ -1,22 +1,10 @@
 import React from 'react'
-import BookmarksSearch from "@/app/components/bookmarks/bookmarks-search";
 import BookmarkTabs from "@/app/components/bookmarks/bookmark-tabs";
-import {trpc} from "@/trpc/server";
 
 const Page = async () => {
-    const bookmarks = await trpc.bookmarks.getUserBookmarks();
-
     return (
         <div className={'mt-10'}>
-            <div className={'flex items-center justify-between w-full'}>
-                <h2 className={'font-bold text-2xl'}>
-                    Bookmarks
-                </h2>
-                <BookmarksSearch/>
-            </div>
-            <div className={'mt-8 w-full'}>
-                <BookmarkTabs bookmarks={bookmarks}/>
-            </div>
+            <BookmarkTabs/>
         </div>
     )
 }
--- app/components/bookmarks/bookmark-tabs.tsx
+++ app/components/bookmarks/bookmark-tabs.tsx
@@ -3,30 +3,39 @@
 import React from 'react'
 import {Tabs, TabsContent, TabsList, TabsTrigger} from "@/components/ui/tabs";
 import BookmarkList from "@/app/components/bookmarks/bookmark-list";
-import {RouterOutputs} from "@/trpc/routers/_app";
+import BookmarksSearch from "@/app/components/bookmarks/bookmarks-search";
+import {trpc} from "@/trpc/client";
 
-type Props = {
-    bookmarks:RouterOutputs['bookmarks']['getUserBookmarks']
-}
+const BookmarkTabs = () => {
+    const {data:bookmarks} = trpc.bookmarks.getUserBookmarks.useQuery();
 
-const BookmarkTabs = ({bookmarks}:Props) => {
     return (
-        <Tabs defaultValue="all" className={'w-full'}>
-            <TabsList>
-                <TabsTrigger value="all">All</TabsTrigger>
-                <TabsTrigger value="movies">Movies</TabsTrigger>
-                <TabsTrigger value="tv">Tv Series</TabsTrigger>
-            </TabsList>
-            <TabsContent value="all" className={'w-full'}>
-                <BookmarkList bookmarks={[...bookmarks]}/>
-            </TabsContent>
-            <TabsContent value="movies" className={'w-full'}>
-                <BookmarkList bookmarks={[...bookmarks.filter((bookmark) => bookmark.mediaType === 'movie')]}/>
-            </TabsContent>
-            <TabsContent value="tv" className={'w-full'}>
-                <BookmarkList bookmarks={[...bookmarks.filter((bookmark) => bookmark.mediaType === 'tv')]}/>
-            </TabsContent>
-        </Tabs>
+        <div>
+            <div className={'flex items-center justify-between w-full'}>
+                <h2 className={'font-bold text-2xl'}>
+                    Bookmarks
+                </h2>
+                <BookmarksSearch/>
+            </div>
+            <div className={'mt-8 w-full'}>
+                <Tabs defaultValue="all" className={'w-full'}>
+                    <TabsList>
+                        <TabsTrigger value="all">All</TabsTrigger>
+                        <TabsTrigger value="movies">Movies</TabsTrigger>
+                        <TabsTrigger value="tv">Tv Series</TabsTrigger>
+                    </TabsList>
+                    <TabsContent value="all" className={'w-full'}>
+                        <BookmarkList bookmarks={bookmarks ?? []}/>
+                    </TabsContent>
+                    <TabsContent value="movies" className={'w-full'}>
+                        <BookmarkList bookmarks={bookmarks?.filter((bookmark) => bookmark.mediaType === 'movie') ?? []}/>
+                    </TabsContent>
+                    <TabsContent value="tv" className={'w-full'}>
+                        <BookmarkList bookmarks={bookmarks?.filter((bookmark) => bookmark.mediaType === 'tv') ?? []}/>
+                    </TabsContent>
+                </Tabs>
+            </div>
+        </div>
     )
 }
 export default BookmarkTabs
