283d57be73943ffa1b82affcc6821663cbe197c2
Johnson Mbeba <101251043+jjmbeba@users.noreply.github.com>
2024-12-03 23:38:10 +0300

    Add bookmark filter by title functionality

--- app/components/bookmarks/bookmark-tabs.tsx
+++ app/components/bookmarks/bookmark-tabs.tsx
@@ -1,13 +1,18 @@
 "use client";
 
-import React from 'react'
+import React, {useMemo, useState} from 'react'
 import {Tabs, TabsContent, TabsList, TabsTrigger} from "@/components/ui/tabs";
 import BookmarkList from "@/app/components/bookmarks/bookmark-list";
 import BookmarksSearch from "@/app/components/bookmarks/bookmarks-search";
 import {trpc} from "@/trpc/client";
 
 const BookmarkTabs = () => {
-    const {data:bookmarks} = trpc.bookmarks.getUserBookmarks.useQuery();
+    const {data: bookmarks} = trpc.bookmarks.getUserBookmarks.useQuery();
+    const [search, setSearch] = useState('venom');
+
+    const filteredBookmarks = useMemo(() => {
+        return bookmarks?.filter((bookmark) => bookmark.title.toLowerCase().includes(search.toLowerCase()))
+    }, [search, bookmarks]);
 
     return (
         <div>
@@ -15,7 +20,7 @@ const BookmarkTabs = () => {
                 <h2 className={'font-bold text-2xl'}>
                     Bookmarks
                 </h2>
-                <BookmarksSearch/>
+                <BookmarksSearch setSearch={setSearch}/>
             </div>
             <div className={'mt-8 w-full'}>
                 <Tabs defaultValue="all" className={'w-full'}>
@@ -25,13 +30,15 @@ const BookmarkTabs = () => {
                         <TabsTrigger value="tv">Tv Series</TabsTrigger>
                     </TabsList>
                     <TabsContent value="all" className={'w-full'}>
-                        <BookmarkList bookmarks={bookmarks ?? []}/>
+                        <BookmarkList bookmarks={filteredBookmarks ?? []}/>
                     </TabsContent>
                     <TabsContent value="movies" className={'w-full'}>
-                        <BookmarkList bookmarks={bookmarks?.filter((bookmark) => bookmark.mediaType === 'movie') ?? []}/>
+                        <BookmarkList
+                            bookmarks={filteredBookmarks?.filter((bookmark) => bookmark.mediaType === 'movie') ?? []}/>
                     </TabsContent>
                     <TabsContent value="tv" className={'w-full'}>
-                        <BookmarkList bookmarks={bookmarks?.filter((bookmark) => bookmark.mediaType === 'tv') ?? []}/>
+                        <BookmarkList
+                            bookmarks={filteredBookmarks?.filter((bookmark) => bookmark.mediaType === 'tv') ?? []}/>
                     </TabsContent>
                 </Tabs>
             </div>
--- app/components/bookmarks/bookmarks-search.tsx
+++ app/components/bookmarks/bookmarks-search.tsx
@@ -2,10 +2,14 @@ import React from 'react'
 import {Input} from "@/components/ui/input";
 import {CornerDownLeft} from "lucide-react";
 
-const BookmarksSearch = () => {
+type Props = {
+    setSearch: React.Dispatch<React.SetStateAction<string>>
+}
+
+const BookmarksSearch = ({setSearch}: Props) => {
     return (
         <div className={'max-w-64 relative'}>
-            <Input placeholder={'Search bookmarks'} className={'pr-8'}/>
+            <Input onChange={(e) => setSearch(e.target.value)} placeholder={'Search bookmarks'} className={'pr-8'}/>
             <CornerDownLeft className={'h-4 w-4 absolute right-3 top-1/2 -translate-y-1/2'}/>
         </div>
     )
