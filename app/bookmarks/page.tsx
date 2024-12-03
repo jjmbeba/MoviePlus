4312fe48034efd831ff24f7902fe81fcf5565917
Johnson Mbeba <101251043+jjmbeba@users.noreply.github.com>
2024-12-03 23:11:55 +0300

    refactor bookmarks

--- app/bookmarks/page.tsx
+++ app/bookmarks/page.tsx
@@ -1,11 +1,8 @@
 import React from 'react'
-import {trpc} from "@/trpc/server";
-import {Tabs, TabsContent, TabsList, TabsTrigger} from "@/components/ui/tabs";
-import BookmarkList from "@/app/components/bookmarks/bookmark-list";
 import BookmarksSearch from "@/app/components/bookmarks/bookmarks-search";
 
 const Page = async () => {
-    const bookmarks = await trpc.bookmarks.getUserBookmarks();
+    // const bookmarks = await trpc.bookmarks.getUserBookmarks();
 
     return (
         <div className={'mt-10'}>
@@ -16,22 +13,7 @@ const Page = async () => {
                 <BookmarksSearch/>
             </div>
             <div className={'mt-8 w-full'}>
-                <Tabs defaultValue="all" className={'w-full'}>
-                    <TabsList>
-                        <TabsTrigger value="all">All</TabsTrigger>
-                        <TabsTrigger value="movies">Movies</TabsTrigger>
-                        <TabsTrigger value="tv">Tv Series</TabsTrigger>
-                    </TabsList>
-                    <TabsContent value="all" className={'w-full'}>
-                        <BookmarkList bookmarks={[...bookmarks]}/>
-                    </TabsContent>
-                    <TabsContent value="movies" className={'w-full'}>
-                        <BookmarkList bookmarks={[...bookmarks.filter((bookmark) => bookmark.mediaType === 'movie')]}/>
-                    </TabsContent>
-                    <TabsContent value="tv" className={'w-full'}>
-                        <BookmarkList bookmarks={[...bookmarks.filter((bookmark) => bookmark.mediaType === 'tv')]}/>
-                    </TabsContent>
-                </Tabs>
+
             </div>
         </div>
     )
