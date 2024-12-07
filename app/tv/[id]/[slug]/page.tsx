de3d5380a6716db1b566f1971fff26c23e653c44
Johnson Mbeba <101251043+jjmbeba@users.noreply.github.com>
2024-12-07 17:34:49 +0300

    Display image on series detail page

--- app/tv/[id]/[slug]/page.tsx
+++ app/tv/[id]/[slug]/page.tsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import BackButton from "@/app/components/back-button";
-import {Card, CardContent} from "@/components/ui/card";
 import BookmarkButton from "@/app/components/bookmark-button";
 import {trpc} from "@/trpc/server";
 import Recommendations from "@/app/components/recommendations";
 import ReviewsList from "@/app/components/reviews/reviews-list";
 import SeriesStats from "@/app/components/tv/series-stats";
 import {auth} from "@clerk/nextjs/server";
+import Image from "next/image";
+import {generateImageUrl} from "@/lib/utils";
 
 type Props = {
     params: Promise<{ slug: string; id: string; }>
@@ -23,15 +24,13 @@ const Page = async ({params}: Props) => {
 
     const {name, overview, id, vote_average, number_of_seasons, number_of_episodes, genres, poster_path, backdrop_path} = series;
 
+    const imageUrl = poster_path === '' ? 'https://placehold.co/400' : generateImageUrl(poster_path);
+
     return (
         <div className={'mt-10'}>
             <BackButton/>
-            <div className={'flex items-start gap-16 mt-8'}>
-                <Card className={'relative w-1/2 h-[60vh] flex items-center justify-center'}>
-                    <CardContent className="flex aspect-square items-center justify-center w-1/3">
-                        Image
-                    </CardContent>
-                </Card>
+            <div className={'flex items-start justify-center gap-28 mt-8'}>
+                <Image className={'rounded-xl'} width={327} height={412} src={imageUrl} alt={name}/>
                 <div>
                     <div className={'flex items-center justify-between'}>
                         <h1 className={'text-2xl'}>
